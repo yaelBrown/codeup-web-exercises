08bce03960ecdc703917c7aea346eb1eb51d8904
yaelBrown <yaelrbrown@gmail.com>
2019-08-15 14:47:23 -0500

    Added defuse the bom bonuses

---
+++ js/defuse-the-bom.js
@@ -0,0 +1,19 @@
+function sayHello() {
+  console.log("Hello Ceres!");
+}
+
+function sayGoodbye() {
+  console.log("Goodbye Ceres!");
+}
+
+// var a = setInterval(sayHello, 1000);
+
+/*
+  It executes and stores the return into the variable. 
+*/
+
+
+
+// setInterval(sayGoodbye, 900);
+
+// console.log(setInterval(sayGoodbye, 1100));
