c51b80995119a2124a6669ae726267d923823d7c
yaelBrown <yaelrbrown@gmail.com>
2019-08-19 15:42:36 -0500

    finished mapbox exercises

---
+++ js/mapbox-utils.js
@@ -0,0 +1,71 @@
+console.log("mapbox-utils");
+
+"use strict";
+
+/***
+ * geocode is a method to search for coordinates based on a physical address and return
+ * @param {string} search is the address to search for the geocoded coordinates
+ * @param {string} token is your API token for MapBox
+ * @returns {Promise} a promise containing the latitude and longitude as a two element array
+ *
+ * EXAMPLE:
+ *
+ *  geocode("San Antonio", API_TOKEN_HERE).then(function(results) {
+ *      // do something with results
+ *  })
+ *
+ */
+
+function geocode(search, token, loc) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+            return data.features[0].center;
+        });
+}
+
+/***
+ * reverseGeocode is a method to search for a physical address based on inputted coordinates
+ * @param {object} coordinates is an object with properties "lat" and "lng" for latitude and longitude
+ * @param {string} token is your API token for MapBox
+ * @returns {Promise} a promise containing the string of the closest matching location to the coordinates provided
+ *
+ * EXAMPLE:
+ *
+ *  reverseGeocode({lat: 32.77, lng: -96.79}, API_TOKEN_HERE).then(function(results) {
+ *      // do something with results
+ *  })
+ *
+ */
+
+function reverseGeocode(coordinates, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+        })
+        // to get all the data from the request, comment out the following three lines...
+        .then(function(data) {
+            return data.features[0].place_name;
+        });
+}
+
+var miami;
+geocode("Miami, Florida", mapBoxToken).then(function(result) { miami = result} );
+
+/* access info of miami
+
+miami[0] gets longitude
+miami[1] gets lattitude
+
+*/
+
+// Reverse geocodiing is used for providing your add and to find the nearest store or if you want to know where you are. 
+
+
+// map.flyTo() - flies you to location 
