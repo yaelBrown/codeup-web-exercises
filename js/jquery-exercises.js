94c9eed446bd3eb5df25cb0bd61895a165d12158
yaelBrown <yaelrbrown@gmail.com>
2019-08-23 18:53:02 -0500

    started jquery exercises

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,5 +1,27 @@
 "use strict";
 
+
 $(document).ready(function() {
-  alert("DOM has finished loading.");
+  // alert("DOM has finished loading.");
+  // alert($('#para').html());
+  // alert($('#h3tag').html());
+  $('.codeup').css("border", "1px solid red");
 });
+
+// $('#h3tag').css("background-color", "green");
+$('li').css("font-size", "20px");
+$('#para').removeClass("codeup");
+
+// $('h1')
+// $('p')
+// $('li')
+
+// alert($('h1').html());
+
+// multiple selectors
+$('h1, p, li')
+
+
+// selects first item if 2 of the same id tags are used. Ignores the rest.
+
+// element does not get the border, because gets border
